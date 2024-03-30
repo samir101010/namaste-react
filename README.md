8e8169899319d53917a80bd7f62d851d035865de
Samir Chanda <samir.82d@hotmail.com>
2024-03-30 18:37:01 +0100

    more code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Namaste React 🚀
+
+# Basic React Understanding
--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const heading = React.createElement(
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "i am h1 sibling"),
     React.createElement("h2", {}, "i am h2 sibling"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "i am h1 sibling"),
+    React.createElement("h2", {}, "i am h2 sibling"),
   ])
 );
 
