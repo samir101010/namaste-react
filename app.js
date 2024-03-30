4cb0061b50d2124040ef94ab18c1453258ca97b8
Samir Chanda <samir.82d@hotmail.com>
2024-03-30 15:20:06 +0100

    episode-01

---
+++ app.js
@@ -0,0 +1,16 @@
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am h1 sibling"),
+    React.createElement("h2", {}, "i am h2 sibling"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "i am h1 sibling"),
+    React.createElement("h2", {}, "i am h2 sibling"),
+  ])
+);
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
